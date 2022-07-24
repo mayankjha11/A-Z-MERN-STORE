cad437c1461692d1aad2d844dba15c923187b153
mayankjha11 <mjha0040@gmail.com>
2022-07-24 14:24:48 +0530

    first commit

---
+++ dbConnect.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const URL = process.env.MONGODB_URI;
+
+mongoose.connect(URL)
+
+let connectionObj = mongoose.connection
+
+connectionObj.on('connected' , ()=>{
+    console.log('Mongo DB Connection Successfull')
+})
+
+connectionObj.on('error' , ()=>{
+    console.log('Mongo DB Connection Failed')
+})
